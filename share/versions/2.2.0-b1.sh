a9e2d106f8efd6b1c17aa8fc3294ee359d2cce96
Stephane Chamberland <stephane.chamberland@ec.gc.ca>
2023-06-19 15:51:30 +0000

    Update version (2.2.0-rc1)

---
+++ share/versions/2.2.0-b1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/fs/homeu2/eccc/mrd/rpnatm/sch001/data/ords/gitdepot/python-rpn/bin/.env_setup.dot rpnpy 2.2.0-b1 all /fs/homeu2/eccc/mrd/rpnatm/sch001/data/ords/gitdepot/python-rpn /fs/homeu2/eccc/mrd/rpnatm/sch001/data/ords/gitdepot/python-rpn 
