5962dc9b6cb8e53cf387586506208f9cd4dd4257
Stephane Chamberland <stephane.chamberland@ec.gc.ca>
2023-06-21 13:53:06 +0000

    path typo

--- share/versions/2.2.0-rc1.sh
+++ share/versions/2.2.0-rc1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 __rpnphy_version=x/2.2.0-rc1
-__rpnpy_basedir=/fs/ssm/eccc/mrb/rpn/MIG/ENV/d/rpnpy/${__rpnphy_version}
+__rpnpy_basedir=/fs/ssm/eccc/mrd/rpn/MIG/ENV/d/rpnpy/${__rpnphy_version}
 ${__rpnpy_basedir}/bin/.env_setup.dot rpnpy ${__rpnphy_version##x/} all ${__rpnpy_basedir} ${__rpnpy_basedir}
 unset __rpnphy_version __rpnphy_basedir
