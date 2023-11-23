a72c862d37c28e9b9a67e7c8aa590c292808397e
Stephane Chamberland <stephane.chamberland@ec.gc.ca>
2023-11-23 14:49:40 +0000

    Update to version 2.2.0-rc3

---
+++ lib/rpnpy/version.py
@@ -0,0 +1,2 @@
+__VERSION__ = '2.2.0-rc3'
+__LASTUPDATE__ = '2023-11-23 14:46 GMT'
--- share/versions/2.2.0-rc3.sh
+++ share/versions/2.2.0-rc3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-__rpnpy_version=2.2.0-rc2
+__rpnpy_version=2.2.0-rc3
 __rpnpy_x=x/
 __rpnpy_basedir=/fs/ssm/eccc/mrd/rpn/MIG/ENV/d/rpnpy/${__rpnpy_x}${__rpnpy_version}
 . ${__rpnpy_basedir}/bin/.env_setup.dot rpnpy ${__rpnpy_version##x/} all ${__rpnpy_basedir} ${__rpnpy_basedir}
